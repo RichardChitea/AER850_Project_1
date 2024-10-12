a8ddc0a96994ba5bdca007a54d7c9bbf18a28c14
RichardChitea <richard.chitea@torontomu.ca>
2024-10-12 11:16:58 -0400

    Data Processing and Preliminary Visualization

--- AER850_Project_1.py
+++ AER850_Project_1.py
@@ -1,2 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+#Reading csv into dataframe
+df = pd.read_csv("Project_1_Data.csv")
+
+#print(df.info())
+
+# Histograms
+df.hist()
+
+#dfnp = df.to_numpy()
+
+#3d plot
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(df['X'],df['Y'],df['Z'])
+plt.show()
+
+
+a_columns = ['X',
+             'Y',
+             'Z']
+b_columns = ['Step']
+
+a = df[a_columns] 
+b = df[b_columns]
 
