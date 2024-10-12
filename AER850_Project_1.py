ae917ac98a753d636a0f69b92e87fcb60f6dce4c
RichardChitea <richard.chitea@torontomu.ca>
2024-10-12 13:08:32 -0400

    Data Visualization and Correlation Analysis

--- AER850_Project_1.py
+++ AER850_Project_1.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from pandas.plotting import scatter_matrix
+
 #Reading csv into dataframe
 df = pd.read_csv("Project_1_Data.csv")
 
@@ -19,11 +21,21 @@ for grp_name, grp_idx in df.groupby('Step').groups.items():
     y = df.iloc[grp_idx,1]
     z = df.iloc[grp_idx,2]
     ax.scatter3D(x, y, z, label=grp_name)
-
-ax.legend(bbox_to_anchor=(1,0.5),loc='center left',frameon=False)
+ax.legend(bbox_to_anchor=(1.2,0.5),loc='center left',frameon=False)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 plt.show()
 
+#Attempting to find other patterns
+attributes = ["X","Y","Z","Step"]
+scatter_matrix(df[attributes])
 
+#Using Pearson's r
+corr_matrix = df.corr()
+print(corr_matrix["Step"].sort_values(ascending=False))
+
+#
 a_columns = ['X',
              'Y',
              'Z']
